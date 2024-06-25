b03f7a4f54bd66261663a78cbc67223f85608541
jeong724 <oliveyou724@gmail.com>
2024-06-25 19:24:33 +0900

    로그인 후 화면 전환 + 로그인 토글 버튼

--- Desktop/clone_projects/nwitter/src/routes/Auth.js
+++ Desktop/clone_projects/nwitter/src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
         //console.log(event.target.name);
@@ -13,7 +14,7 @@ const Auth = () =>{
         } = event;
         if(name == "email"){
             setEmail(value);
-            console.log(event.target.value)
+            console.log(value)
         } else if(name == "password"){
             setPassword(value);
         }
@@ -31,9 +32,11 @@ const Auth = () =>{
         }
         console.log(data);
     } catch(error){
-        console.log(error);
+        setError(error.message);
     }
-    }
+    };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return(
         <div>
@@ -53,7 +56,11 @@ const Auth = () =>{
                 onChange={onChange}
                 />
                 <input type="submit" value={newAccount ? "create account" : "log in"}/>
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
         <div>
             <button>Continue with Google</button>
             <button>Continue with Github</button>
