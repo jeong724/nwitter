bc225073c58d7f104f3c3334d3a52355f33e6568
jeong724 <oliveyou724@gmail.com>
2024-07-02 16:49:32 +0900

    트윗 수정

--- Desktop/clone_projects/nwitter/src/components/Nweet.js
+++ Desktop/clone_projects/nwitter/src/components/Nweet.js
@@ -1,6 +1,10 @@
 import { dbService } from "fbase";
+import { useState } from "react";
 
 const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
     const onDeleteClick = async () =>{
         const ok = window.confirm("삭제하시겠습니까?");
         console.log(ok);
@@ -12,15 +16,44 @@ const Nweet = ({nweetObj, isOwner}) => {
         }
     };
 
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onChange = (event) =>{
+        const{
+            target: {value},
+        } = event;
+        setNewNweet(value);
+        
+    };
+
+    const onSubmit = async (event) =>{
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({text : newNweet});
+        setEditing(false);
+    }
+
     return(
         <div>
+            {editing ? (
+                <>
+                <form onSubmit={onSubmit}>
+                    <input onChange={onChange} value={newNweet} required/>
+                    <input type="submit" value="update Nweet"/>
+                </form>
+                <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+        
             <h4>
                 {nweetObj.text}
             </h4>
             {isOwner &&(
                 <>
             <button onClick = {onDeleteClick}>Delete Nweet</button>
-            <button>Edit NWeet</button>
+            <button onClick={toggleEditing}>Edit NWeet</button>
+            </>
+            )}
             </>
             )}
         </div>
