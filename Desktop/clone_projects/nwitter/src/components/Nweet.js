a29379327240caa155c29f722424694b5a5a3205
jeong724 <oliveyou724@gmail.com>
2024-07-02 16:23:39 +0900

    트윗 삭제 기능 추가

---
+++ Desktop/clone_projects/nwitter/src/components/Nweet.js
@@ -0,0 +1,30 @@
+import { dbService } from "fbase";
+
+const Nweet = ({nweetObj, isOwner}) => {
+    const onDeleteClick = async () =>{
+        const ok = window.confirm("삭제하시겠습니까?");
+        console.log(ok);
+
+        if(ok){
+            console.log(nweetObj.id);
+            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            console.log(data);
+        }
+    };
+
+    return(
+        <div>
+            <h4>
+                {nweetObj.text}
+            </h4>
+            {isOwner &&(
+                <>
+            <button onClick = {onDeleteClick}>Delete Nweet</button>
+            <button>Edit NWeet</button>
+            </>
+            )}
+        </div>
+    );
+};
+
+export default Nweet;
--- Desktop/clone_projects/nwitter/src/routes/Home.js
+++ Desktop/clone_projects/nwitter/src/routes/Home.js
@@ -1,11 +1,10 @@
 import { useEffect,useState } from "react";
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({userObj}) =>{
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-
-
 
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) =>{
@@ -51,9 +50,9 @@ const Home = ({userObj}) =>{
         </form>
         <div>
             {nweets.map((nweet)=>(
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                    </div>
+                <Nweet key={nweet.id} 
+                nweetObj={nweet}
+                isOwner = {nweet.creatorId === userObj.uid}/>
             ))}
             </div>
         </>
